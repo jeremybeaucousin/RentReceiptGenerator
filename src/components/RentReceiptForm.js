899e2c43d3662e31ca16afef6d38278ad536a8c3
Jeremy BEAUCOUSIN <beaucousin.jeremy@valeuriad.fr>
2020-07-16 12:23:17 +0200

    Handle state for formulaire

--- src/components/RentReceiptForm.js
+++ src/components/RentReceiptForm.js
@@ -1,23 +1,38 @@
 import React from 'react';
 
 import { FormGroup, FormControl } from 'react-bootstrap';
+import './RentReceiptForm.css';
 
 export default class RentReceiptForm extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { value: '' };
+        this.state = {
+            'ownerFirstName' : "Jonathan",
+            'ownerLastName' : "BEAUCOUSIN",
+            'tenantFirstName' : "Léa",
+            'tenantLastName' : "LIMOGES",
+            'adresse' : "4ème étage\n187 rue de verdun\n76600 Le havre\nFrance",
+            'dateTransmission' : "2020-07-02",
+            'periodeStart' : "2020-07-02",
+            'periodeEnd' : "2020-07-02",
+            'rent' : 450,
+            'charges' : 0
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({ value: event.target.value });
+        const value = event.target.value;
+        console.log(value);
+        console.log(event.target.name);
+        this.setState({ [event.target.name]: event.target.value });
     }
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
+        alert('A name was submitted: ' + this.state);
         event.preventDefault();
     }
 
@@ -27,12 +42,12 @@ export default class RentReceiptForm extends React.Component {
                 <FormGroup className="row">
                     <div className="col-sm-6">
                         <label htmlFor="RentReceiptFormOwnerFirstName"> Prénom du propriétaire : </label>
-                        <FormControl type="text" id="RentReceiptFormOwnerFirstName" value={this.state.value} onChange={this.handleChange} />
+                        <FormControl type="text" name="ownerFirstName" id="RentReceiptFormOwnerFirstName" value={this.state.ownerFirstName} onChange={this.handleChange} />
                     </div>
 
                     <div className="col-sm-6">
                         <label htmlFor="RentReceiptFormOwnerLastName"> Nom du propriétaire : </label>
-                        <FormControl type="text" id="RentReceiptFormOwnerLastName" value={this.state.value} onChange={this.handleChange} />
+                        <FormControl type="text" name="ownerLastName" id="RentReceiptFormOwnerLastName" value={this.state.ownerLastName} onChange={this.handleChange} />
                     </div>
 
                 </FormGroup>
@@ -40,51 +55,46 @@ export default class RentReceiptForm extends React.Component {
                 <FormGroup className="row">
                     <div className="col-sm-6">
                         <label htmlFor="RentReceiptFormTenantFirstName"> Prénom du locataire : </label>
-                        <FormControl type="text" id="RentReceiptFormTenantFirstName" value={this.state.value} onChange={this.handleChange} />
+                        <FormControl type="text" name="tenantFirstName" id="RentReceiptFormTenantFirstName" value={this.state.tenantFirstName} onChange={this.handleChange} />
                     </div>
                     <div className="col-sm-6">
                         <label htmlFor="RentReceiptFormTenantLastName"> Nom du locataire : </label>
-                        <FormControl type="text" id="RentReceiptFormTenantLastName" value={this.state.value} onChange={this.handleChange} />
+                        <FormControl type="text" name="tenantLastName" id="RentReceiptFormTenantLastName" value={this.state.tenantLastName} onChange={this.handleChange} />
                     </div>
                 </FormGroup>
 
                 <FormGroup >
                     <label htmlFor="RentReceiptFormAdresse"> Adresse du bien : </label>
-                    <FormControl type="text" id="RentReceiptFormAdresse" value={this.state.value} onChange={this.handleChange} />
+                    <textarea className="form-control textarea-autosize" name="adresse" id="RentReceiptFormAdresse" value={this.state.adresse} onChange={this.handleChange} />
                 </FormGroup>
 
                 <FormGroup >
                     <label htmlFor="RentReceiptFormDateTransmission"> Date d'émission de la quittance : </label>
-                    <FormControl type="date" id="RentReceiptFormDateTransmission" value={this.state.value} onChange={this.handleChange} />
+                    <FormControl type="date" name="dateTransmission" id="RentReceiptFormDateTransmission" value={this.state.dateTransmission} onChange={this.handleChange} />
                 </FormGroup>
 
                 <FormGroup className="row">
-                <div className="col-sm-6">
-                <label htmlFor="RentReceiptFormPeriodeStart"> Début de la période concernée : </label>
-                    <FormControl type="date" id="RentReceiptFormPeriodeStart" value={this.state.value} onChange={this.handleChange} />
+                    <div className="col-sm-6">
+                        <label htmlFor="RentReceiptFormPeriodeStart"> Début de la période concernée : </label>
+                        <FormControl type="date" name="periodeStart" id="RentReceiptFormPeriodeStart" value={this.state.periodeStart} onChange={this.handleChange} />
                     </div>
                     <div className="col-sm-6">
-                    <label htmlFor="RentReceiptFormPeriodeEnd"> Fin de la période concernée : </label>
-                    <FormControl type="date" id="RentReceiptFormPeriodeEnd" value={this.state.value} onChange={this.handleChange} />
+                        <label htmlFor="RentReceiptFormPeriodeEnd"> Fin de la période concernée : </label>
+                        <FormControl type="date" name="periodeEnd" id="RentReceiptFormPeriodeEnd" value={this.state.periodeEnd} onChange={this.handleChange} />
                     </div>
 
                 </FormGroup>
 
                 <FormGroup >
                     <label htmlFor="RentReceiptFormRent"> Loyer mensuel contractuel : </label>
-                    <FormControl type="number" id="RentReceiptFormRent" value={this.state.value} onChange={this.handleChange} />
+                    <FormControl type="number" name="rent" id="RentReceiptFormRent" value={this.state.rent} onChange={this.handleChange} />
                 </FormGroup>
 
                 <FormGroup >
-                    <label htmlFor="RentReceiptFormCharge"> Charges mensuelles contractuelles : </label>
-                    <FormControl type="number" id="RentReceiptFormCharge" value={this.state.value} onChange={this.handleChange} />
+                    <label htmlFor="RentReceiptFormCharges"> Charges mensuelles contractuelles : </label>
+                    <FormControl type="number" name="charge" id="RentReceiptFormCharges" value={this.state.charges} onChange={this.handleChange} />
                 </FormGroup>
 
-                <FormGroup >
-                    <label htmlFor="RentReceiptFormSum"> Sommes payées au bailleur : </label>
-                    <FormControl type="text" id="RentReceiptFormSum" value={this.state.value} onChange={this.handleChange} />
-
-                </FormGroup>
                 <input type="submit" value="Générer" />
             </form>
         );
