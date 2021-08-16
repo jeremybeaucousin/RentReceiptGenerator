ca5a7f88990f7f4a5636e1a632bd3f221f46c2bb
BCNJ <jeremy.beaucousin@cnieg.fr>
2021-08-16 07:03:48 +0200

    Change tenant from child form

--- src/components/TenantForm.js
+++ src/components/TenantForm.js
@@ -6,6 +6,9 @@ export class TenantForm extends React.Component {
     constructor(props) {
         super(props);
         this.handleTenantChange = props.handleTenantChange;
+        this.state = {
+            tenant : props.tenant
+        }
     }
 
     render() {
@@ -14,17 +17,17 @@ export class TenantForm extends React.Component {
                 <FormGroup className="row">
                     <div className="col-sm-6">
                         <label htmlFor="firstName"> Prénom du locataire : </label>
-                        <FormControl type="text" name="firstName" id="firstName" value={this.props.tenant.firstName} onChange={this.handleTenantChange} />
+                        <FormControl type="text" name="firstName" id="firstName" value={this.state.tenant.firstName} onChange={this.handleTenantChange} />
                     </div>
                     <div className="col-sm-6">
                         <label htmlFor="lastName"> Nom du locataire : </label>
-                        <FormControl type="text" name="lastName" id="lastName" value={this.props.tenant.lastName} onChange={this.handleTenantChange} />
+                        <FormControl type="text" name="lastName" id="lastName" value={this.state.tenant.lastName} onChange={this.handleTenantChange} />
                     </div>
                 </FormGroup>
 
                 <FormGroup >
                     <label htmlFor="adresse"> Adresse du locataire : </label>
-                    <textarea className="form-control textarea-autosize" name="adress" id="adresse" value={this.props.tenant.adress} onChange={this.handleTenantChange} />
+                    <textarea className="form-control textarea-autosize" name="adress" id="adresse" value={this.state.tenant.adress} onChange={this.handleTenantChange} />
                 </FormGroup>
             </div>
         );
