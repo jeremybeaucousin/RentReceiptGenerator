1a83ce751b6651d891db4eed295c8ef1306ac16d
BCNJ <jeremy.beaucousin@cnieg.fr>
2021-09-26 18:33:52 +0200

    Display data on admin interface

--- src/components/TenantAdmin.js
+++ src/components/TenantAdmin.js
@@ -1,26 +1,76 @@
 import React from 'react';
 
+import { Button, Accordion, Card, useAccordionToggle } from 'react-bootstrap';
+
+import { getSessionCookie } from "../model/Session";
+
+// function CustomToggle({ children, eventKey }) {
+//     const decoratedOnClick = useAccordionToggle(eventKey, () =>
+//       console.log('totally custom!'),
+//     );
+  
+//     return (
+//       <button
+//         type="button"
+//         style={{ backgroundColor: 'pink' }}
+//         onClick={decoratedOnClick}
+//       >
+//         {children}
+//       </button>
+//     );
+//   }
 export class TenantAdmin extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          error: null,
-          isLoaded: false,
-          users: []
+            error: null,
+            isLoaded: false,
+            tenantsLoaded: false,
+            properties: [],
+            tenants: []
         };
-      }
 
-    componentDidMount() {
-        fetch(process.env.REACT_APP_RENT_RECEIPT_API_URL + "/users")
-            .then(res => res.json())
+        this.loadTenants = this.loadTenants.bind(this);
+    }
+
+    loadTenants(event) {
+        const owner = getSessionCookie();
+        let propertyId = event.target.dataset.propertyId;
+        fetch(`${process.env.REACT_APP_RENT_RECEIPT_API_URL}owners/${owner.ID}/properties/${propertyId}/tenants`)
             .then(
                 (result) => {
-                    this.setState({
-                        isLoaded: true,
-                        users: result
-                    });
+                    result.json()
+                        .then((data) => {
+                            this.setState({
+                                tenantsLoaded: true,
+                                tenants: data
+                            });
+                        })
                 },
 
+                (error) => {
+                    console.error(error);
+                    this.setState({
+                        tenantsLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    componentDidMount() {
+        const owner = getSessionCookie();
+        fetch(`${process.env.REACT_APP_RENT_RECEIPT_API_URL}owners/${owner.ID}/properties`)
+            .then(
+                (result) => {
+                    result.json()
+                        .then((data) => {
+                            this.setState({
+                                isLoaded: true,
+                                properties: data
+                            });
+                        })
+                },
                 (error) => {
                     console.error(error);
                     this.setState({
@@ -30,21 +80,46 @@ export class TenantAdmin extends React.Component {
                 }
             )
     }
+
+    tenantsRender() {
+        const { tenantsLoaded, tenants } = this.state;
+        if (!tenantsLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return <ul>
+                {tenants.map((tenant, index) => (
+                    <li key={tenant.ID}>{tenant.firstname} {tenant.lastname} : {tenant.adress}</li>
+                  ))}   
+            </ul>
+        }
+    }
+
     render() {
-        const { error, isLoaded, users } = this.state;
+        const { error, isLoaded, properties } = this.state;
         if (error) {
             return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
             return <div>Loading...</div>;
         } else {
             return (
-                <ul>
-                    {users.map(user => (
-                        <li key={user.id}>
-                            {user.firstname} {user.lastname}
-                        </li>
-                    ))}
-                </ul>
+                <div>
+                    <h3>Liste des biens</h3>
+                    <Accordion defaultActiveKey="0">
+                        {properties.map((property, index) => (
+                            <Card key={property.ID}>
+                                <Card.Header>
+                                    {/* <CustomToggle eventKey="0">Click me!</CustomToggle> */}
+                                    <Accordion.Toggle as={Button} variant="link" onClick={this.loadTenants} eventKey={index + 1} data-property-id={property.ID}>
+                                        {property.name} - {property.adress}
+                                    </Accordion.Toggle>
+                                </Card.Header>
+                                <Accordion.Collapse eventKey={index + 1}>
+                                    <Card.Body>{this.tenantsRender()}</Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        ))}
+                    </Accordion>
+                </div>
             );
         }
     }
