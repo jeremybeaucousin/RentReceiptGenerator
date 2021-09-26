e44160cda2470a46623d91cef053c4b7ea18b31a
BCNJ <jeremy.beaucousin@cnieg.fr>
2021-09-26 21:14:24 +0200

    Update property

--- src/components/PropertiesAdmin.js
+++ src/components/PropertiesAdmin.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import { Button, Accordion, Card } from 'react-bootstrap';
+import { Button, Accordion, Card, Form, FormControl, InputGroup, Row, Col } from 'react-bootstrap';
 
 import { getSessionCookie } from "../model/Session";
 
 import { TenantsAdmin } from './TenantsAdmin';
+
+const REICEPT_API_URL = process.env.REACT_APP_RENT_RECEIPT_API_URL;
 
 export class PropertiesAdmin extends React.Component {
     constructor(props) {
@@ -12,9 +14,32 @@ export class PropertiesAdmin extends React.Component {
         this.state = {
             error: null,
             isLoaded: false,
-            properties: []
+            properties: [],
+            currentProperty: null
         };
+        this.setCurrentProperty = this.setCurrentProperty.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
+    handleChange = event => {
+        let value = event.target.value;
+        const column = event.target.name;
+        this.setState(prevState => {
+            let currentProperty = Object.assign(Object.create(Object.getPrototypeOf(prevState.currentProperty)), prevState.currentProperty);
+            const propertyIndex = this.state.properties.findIndex(property => property.ID === currentProperty.ID);
+            currentProperty[column] = value;
+            this.state.properties[propertyIndex] = currentProperty;
+            return { currentProperty: currentProperty }
+        });
+    }
+
+    setCurrentProperty(event) {
+        const propertyId = parseInt(event.target.dataset.propertyId);
+        const currentProperty = this.state.properties.find(property => property.ID === propertyId);
+        this.setState({
+            currentProperty: currentProperty
+        });
     }
 
     componentDidMount() {
@@ -41,6 +66,27 @@ export class PropertiesAdmin extends React.Component {
             )
     }
 
+    handleSubmit(event) {
+        fetch(`${REICEPT_API_URL}owners/${this.state.currentProperty.owner}/properties/${this.state.currentProperty.ID}`, {
+            method: 'PUT',
+            body: JSON.stringify(this.state.currentProperty)
+        })
+            .then(
+                (result) => {
+                    console.log(result);
+                    result.json()
+                        .then((data) => {
+                            console.log(data);
+                        })
+                },
+
+                (error) => {
+                    console.error(error);
+                }
+            );
+        event.preventDefault()
+    }
+
     render() {
         const { error, isLoaded, properties } = this.state;
         if (error) {
@@ -53,14 +99,44 @@ export class PropertiesAdmin extends React.Component {
                     {properties.map((property, index) => (
                         <Card key={property.ID}>
                             <Card.Header>
-                                {/* <CustomToggle eventKey="0">Click me!</CustomToggle> */}
-                                <Accordion.Toggle as={Button} variant="link" onClick={this.loadTenants} eventKey={index + 1}>
+                                <Accordion.Toggle as={Button} variant="link" onClick={this.setCurrentProperty} data-property-id={property.ID} eventKey={index + 1}>
                                     {property.name} - {property.adress}
                                 </Accordion.Toggle>
                             </Card.Header>
                             <Accordion.Collapse eventKey={index + 1}>
                                 <Card.Body>
-                                    <TenantsAdmin propertyId={property.ID} />
+                                    <Form onSubmit={this.handleSubmit}>
+                                        <Row>
+                                            <Col>
+                                                <InputGroup>
+                                                    <InputGroup.Prepend>
+                                                        <InputGroup.Text id="PropertiesAdminName">Nom du bien :</InputGroup.Text>
+                                                    </InputGroup.Prepend>
+                                                    <FormControl placeholder="Nom du bien" name="name" type="textarea" value={property.name}
+                                                        aria-label="Nom du bien" aria-describedby="PropertiesAdminName" onChange={this.handleChange}
+                                                    />
+                                                </InputGroup>
+                                            </Col>
+                                            <Col>
+                                                <InputGroup>
+                                                    <InputGroup.Prepend>
+                                                        <InputGroup.Text id="PropertiesAdminAdresse">Addresse du bien :</InputGroup.Text>
+                                                    </InputGroup.Prepend>
+                                                    <FormControl placeholder="Addresse du bien" name="adress" type="text" value={property.adress}
+                                                        aria-label="Addresse du bien" aria-describedby="PropertiesAdminAdresse" onChange={this.handleChange}
+                                                    />
+                                                </InputGroup>
+                                            </Col>
+                                            <Col sm={2} align="right">
+                                                <Button variant="primary" type="submit">
+                                                    Enregistrer
+                                            </Button>
+                                            </Col>
+                                        </Row>
+                                    </Form>
+                                    {this.state.currentProperty && this.state.currentProperty.ID === property.ID &&
+                                        <TenantsAdmin propertyId={property.ID} />
+                                    }
                                 </Card.Body>
                             </Accordion.Collapse>
                         </Card>
